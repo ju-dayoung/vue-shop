51ce6f663d9d137592e148c901816ec53a4b328e
dayoungju <jdy912@gmail.com>
2022-01-04 00:24:03 +0900

    수정

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-<h1>Home</h1>
+  <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+    <h1>Home</h1>
+  </div>
 </template>
 
 <style scoped>
--- client/src/views/Product.vue
+++ client/src/views/Product.vue
@@ -17,17 +17,17 @@ loadProduct()
 </script>
 
 <template>
-<h1>제품 상세</h1>
-<div>
-  <button class="bg-gray-200 px-4 py-2 rounded" @click="router.back">뒤로가기</button>
-</div>
-  <ul>
-    <li>ID: {{product.id}}</li>
-    <li>NAME: {{product.name}}</li>
-    <li>price: {{product.price}}</li>
-    <li><img :src="product.imgUrl"></li>
-  </ul>
-
+  <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+    <div>
+      <button class="bg-gray-200 px-4 py-2 rounded" @click="router.back">뒤로가기</button>
+    </div>
+      <ul>
+        <li>ID: {{product.id}}</li>
+        <li>NAME: {{product.name}}</li>
+        <li>price: {{product.price}}</li>
+        <li><img :src="product.imgUrl"></li>
+      </ul>
+  </div>
 
 </template>
 
