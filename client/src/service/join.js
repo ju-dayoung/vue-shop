96c99a1e5e364000ce99f33ae80ee63571713025
dayoungju <jdy912@gmail.com>
2022-01-10 23:59:30 +0900

    아이디 중복

--- client/src/service/join.js
+++ client/src/service/join.js
@@ -2,11 +2,26 @@ import axios from 'axios';
 
 
 async function addUser(user){
-  return await axios.post("http://localhost:5500/user", user,{
-    header:{
-      'Content-Type':'application/json'
-    },
-  })
+  const userList = await checkUser();
+  let email = userList.map(({ email }) => email)
+  console.log(email)
+  if( email.includes(user.email) ){
+    alert('id 중복');
+    return false;
+  } else {
+    alert('가입완료');
+    return await axios.post("http://localhost:5500/user", user,{
+      header:{
+        'Content-Type':'application/json'
+      },
+    })
+  }
+
+}
+
+async function checkUser(){
+  const userData= await axios.get("http://localhost:5500/user");
+  return userData.data;
 }
 
 export {addUser};
