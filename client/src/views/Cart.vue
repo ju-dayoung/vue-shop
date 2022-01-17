1b40a710e0ba4fda46c2bd781c340f720de4a4d7
dayoungju <jdy912@gmail.com>
2022-01-18 00:29:45 +0900

    2022-01-17-2

--- client/src/views/Cart.vue
+++ client/src/views/Cart.vue
@@ -1,21 +1,23 @@
 <script setup>
-  import {computed, onMounted,ref,watch} from 'vue';
+  import { watch, computed, onMounted, ref } from 'vue';
   import {getProduct, getcartItem, operCartQty} from '../service/cart';
   import {useStore} from 'vuex'
 
   const store = useStore();
-  const cartItems = []
+  const cartItems = ref([])
   const carts = computed(() => store.state.cart)
-  
-  // async function setCartItem(cartList){
-  //   cartList.forEach( async (id) => {
-  //     const cart = await getProduct(id)
-  //     const cartQty = await getcartItem(id)
-  //     cart.qty = cartQty.qty
-  //     cartItems.push(cart);
-  //   });
-  //   return cartItems;
-  // }
+
+  //async function setCartItem(cartList){
+    console.log(carts.value);
+    carts.value.forEach( async (id) => {
+      console.log('id',id);
+      const cart = await getProduct(id)
+      const cartQty = await getcartItem(id)
+      cart.qty = cartQty.qty
+      cartItems.value.push(cart);
+    });
+    //return cartItems;
+  //}
   async function plusQty(id, qty){
     qty++;
     return await operCartQty(id, qty)
@@ -26,24 +28,16 @@
   }
 
   // watch(() => carts.value.length, () => {
-  //   console.log(carts.value.length)
   //   carts.value.forEach(async(id) => {
+  //     console.log(id);
   //     const cart = await getProduct(id)
   //     const cartQty = await getcartItem(id)
   //     cart.qty = cartQty.qty
   //     cartItems.push(cart);
   //   })
-  //   return cartItems
+  //   console.log(cartItems, Object.keys(cartItems).length);
+  //   return cartItems;
   // })
-
-  watch(() => carts.value.length, () => {
-    carts.value.forEach( async (id) => {
-      const cart = await getProduct(id)
-      const cartQty = await getcartItem(id)
-      cart.qty = cartQty.qty
-      cartItems.push(cart);
-    })
-  })
 </script>
 <template>
   <div class="container mx-auto mt-10">
@@ -60,36 +54,35 @@
           <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
         </div>
         <!-- item start --> 
+        {{cartItems}}
         <div v-if="cartItems.length === 0">
-          카트에 상품이 없습니다.
+          카트에 상품이 없습니다...
         </div>
         <div v-else>
-        
-
-        <div v-for="cartItem in cartItems" :key="cartItem.id" class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-          <div class="flex w-2/5"> <!-- product -->
-            <div class="w-20">
-              <img class="h-24" alt="" v-bind:src="cartItem.imgUrl">
+          <div v-for="(cartItem) in cartItems" :key="cartItem.id" class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            <div class="flex w-2/5"> <!-- product -->
+              <div class="w-20">
+                <img class="h-24" alt="" v-bind:src="cartItem.imgUrl">
+              </div>
+              <div class="flex flex-col justify-between ml-4 flex-grow">
+                <span class="font-bold text-sm">{{cartItem.id}}</span>
+                <span class="text-red-500 text-xs">{{cartItem.name}}</span>
+                <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
+              </div>
             </div>
-            <div class="flex flex-col justify-between ml-4 flex-grow">
-              <span class="font-bold text-sm">{{cartItem.id}}</span>
-              <span class="text-red-500 text-xs">{{cartItem.name}}</span>
-              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
+            <div class="flex justify-center w-1/5">
+              <svg class="fill-current text-gray-600 w-3" v-on:click.prevent="minusQty(cartItem.id, cartItem.qty)" viewBox="0 0 448 512">
+                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+              </svg>
+              <input class="mx-2 border text-center w-8" type="text" v-bind:value="cartItem.qty">
+              <svg class="fill-current text-gray-600 w-3" v-on:click.prevent="plusQty(cartItem.id, cartItem.qty)" viewBox="0 0 448 512">
+                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+              </svg>
             </div>
+            <span class="text-center w-1/5 font-semibold text-sm">{{cartItem.price}}</span>
+            <span class="text-center w-1/5 font-semibold text-sm">{{cartItem.price * cartItem.qty}}</span>
           </div>
-          <div class="flex justify-center w-1/5">
-            <svg class="fill-current text-gray-600 w-3" v-on:click.prevent="minusQty(cartItem.id, cartItem.qty)" viewBox="0 0 448 512">
-              <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
-            <input class="mx-2 border text-center w-8" type="text" v-bind:value="cartItem.qty">
-            <svg class="fill-current text-gray-600 w-3" v-on:click.prevent="plusQty(cartItem.id, cartItem.qty)" viewBox="0 0 448 512">
-              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-            </svg>
-          </div>
-          <span class="text-center w-1/5 font-semibold text-sm">{{cartItem.price}}</span>
-          <span class="text-center w-1/5 font-semibold text-sm">{{cartItem.price * cartItem.qty}}</span>
-        </div>
-        <!-- item end --> 
+          <!-- item end --> 
         </div>
 
 
